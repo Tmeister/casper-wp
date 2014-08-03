faf09dfcb57d37ff820e95258e7040ea3e25043a
Enrique Chavez <noone@tmeister.net>
2014-08-02 22:59:46 -0500

    Styles

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,600' rel='stylesheet' type='text/css'>
+
 <?php wp_head(); ?>
 </head>
 
